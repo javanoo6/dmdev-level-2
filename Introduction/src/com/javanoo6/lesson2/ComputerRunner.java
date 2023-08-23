23a35942e509c1083599af6aecedb59dd272a1ed
javanoo6 <95355711+javanoo6@users.noreply.github.com>
2023-08-23 23:26:48 +0300

    introduction lesson 3

--- Introduction/src/com/javanoo6/lesson2/ComputerRunner.java
+++ Introduction/src/com/javanoo6/lesson2/ComputerRunner.java
@@ -4,9 +4,18 @@ public class ComputerRunner {
     public static void main(String[] args) {
         //Тип переменной computer - Computer
         //Это экземпляр класса
+        //Computer() - это конструктор без пар-ов
         Computer computer = new Computer();
         //Обращение к свойствам этого объекта
         computer.load();
         computer.printComputerInfo();
+
+        Computer biggerSsdComputer = new Computer(9000);
+        biggerSsdComputer.printComputerInfo();
+        //Теперь при вызове printComputerInfo поле ssd будет иметь значение 9000
+
+        Computer superComputer = new Computer(9000, 32000);
+        superComputer.printComputerInfo();
+
     }
 }
